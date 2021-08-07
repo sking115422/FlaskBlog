41dd9830d7d650f032874b588f13cd4682749aa7
Spencer King <spencerd.king@gmail.com>
2021-08-07 16:43:10 -0400

    added templates

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Spencer King',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'         
+    }, 
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'         
+    }
+
+]
+
 @app.route("/")
-def hello():
-    return "<h1> Hello World! <h1>"
+@app.route("/home")
+def homePage():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def aboutPage():
+    return render_template('about.html', title = 'About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
